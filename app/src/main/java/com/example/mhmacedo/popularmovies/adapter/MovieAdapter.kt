f1f92336663947fa2def9b7afaada8f179b02a78
Mateus Macedo <mhbassomacedo@gmail.com>
2018-11-13 10:23:27 -0200

    Refactoring: Change the name FILM to MOVIE

---
+++ app/src/main/java/com/example/mhmacedo/popularmovies/adapter/MovieAdapter.kt
@@ -0,0 +1,61 @@
+package com.example.mhmacedo.popularmovies.adapter
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.RecyclerView.Adapter
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.mhmacedo.popularmovies.R
+import com.example.mhmacedo.popularmovies.model.Movie
+import kotlinx.android.synthetic.main.film_item.view.*
+
+class MovieAdapter(
+    private val items: List<Movie>,
+    private val context: Context,
+    private val listener: (Movie) -> Unit
+) : Adapter<MovieAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.film_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = items[position]
+        holder.bindView(item, listener)
+    }
+
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        companion object {
+            private var BASE_URL = "http://image.tmdb.org/t/p/"
+            private var SIZE_IMAGE = "w185/"
+        }
+
+
+        fun bindView(
+            item: Movie,
+            listener: (Movie) -> Unit
+        ) = with(itemView) {
+            //iv_film.setImageDrawable(ContextCompat.getDrawable(context, item.imageResourceId))
+
+            val url = BASE_URL + SIZE_IMAGE + "/" + item.poster_path;
+            Glide.with(this)
+                .load(url)
+                .apply(RequestOptions().override(185, 185))
+                .into(iv_film)
+
+
+            setOnClickListener { listener(item) }
+        }
+
+    }
+}
